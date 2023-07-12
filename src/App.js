53c06203e996c75d31b41c92d313f7c30295f3e1
NANDAGOPANKV <nandagopankalpetta365@gmail.com>
2023-07-12 21:56:11 +0530

    initial commit styling completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./app.css";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div">
+      {/* head */}
+      <h2 className="heading">ToDo List.</h2>
+      {/* input */}
+      <div className="taskRowi">
+        <input className="inpField" type="text" />
+        <button className="trBTN">Add</button>
+      </div>
+      {/* newtask */}
+      <div className="taskRow">
+        <div className="task">
+          <p>Lorem ipsum dolor</p>
+          <button className="actionBTN">Done</button>
+          <button className="actionBTN">Delete</button>
+        </div>
+        <div className="task">
+          <p>Lorem ipsum dolor</p>
+          <button className="actionBTN">Done</button>
+          <button className="actionBTN">Delete</button>
+        </div>
+        <div className="task">
+          <p>Lorem ipsum dolor</p>
+          <button className="actionBTN">Done</button>
+          <button className="actionBTN">Delete</button>
+        </div>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
